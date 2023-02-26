d83bea97a0f9524406318c2cce0b1e252e2ad2ca
fahad AbuFaisal <fahad4x4@gmail.com>
2023-02-26 10:57:17 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,51 +1,34 @@
 #!/bin/bash
 # A sample Bash script, by Fahad ALGhathbar
 
-echo -e "\e[31mRebuild-db\e[0m"	
-	echo 
-        sudo eopkg rebuild-db -y 
-		
-			echo -e "\e[31m########################################################\e[0m"	
+echo "Rebuild-db"
+sudo eopkg rebuild-db -y 
+echo "########################################################"
 
-		
-echo -e "\e[31mCheck\e[0m"
-	echo	
-		sudo eopkg check -y
-					echo -e "\e[31m########################################################\e[0m"	
+echo "Check"
+sudo eopkg check -y
+echo "########################################################"
 
-		
-echo -e "\e[31mUpdate-repo\e[0m"
-    echo
-        sudo eopkg update-repo -y
-					echo -e "\e[31m########################################################\e[0m"	
+echo "Update-repo"
+sudo eopkg update-repo -y
+echo "########################################################"
 
-		
-echo -e "\e[31mUpgrade\e[0m"
-	echo
-		sudo eopkg upgrade -y
-					echo -e "\e[31m########################################################\e[0m"	
+echo "Upgrade"
+sudo eopkg upgrade -y
+echo "########################################################"
 
-		
-echo -e "\e[31mFlatpak Update\e[0m"
-	echo
-		sudo flatpak update -y
-					echo -e "\e[31m########################################################\e[0m"	
+echo "Flatpak Update"
+sudo flatpak update -y
+echo "########################################################"
 
-		
-echo -e "\e[31mRemove\e[0m"
-	echo	
-		sudo eopkg rmo -y 
-					echo -e "\e[31m########################################################\e[0m"	
+echo "Remove"
+sudo eopkg rmo -y 
+echo "########################################################"
 
-		
-echo -e "\e[31mClean\e[0m"
-	echo
-		sudo eopkg clean -y 
-					echo -e "\e[31m########################################################\e[0m"	
+echo "Clean"
+sudo eopkg clean -y 
+echo "########################################################"
 
-		
-echo -e "\e[31mcp\e[0m"
-	echo
-		sudo eopkg cp -y
-					echo -e "\e[31m########################################################\e[0m"	
-
+echo "cp"
+sudo eopkg cp -y
+echo "########################################################"
